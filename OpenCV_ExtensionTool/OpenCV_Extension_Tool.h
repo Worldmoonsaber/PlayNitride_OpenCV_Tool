941d6a5f218de9fb9c5e0d95f25d7596d7d25cb5
TPN-TAE600-011\Playuser <worldmoonsaber@gmail.com>
2024-09-02 09:22:08 +0800

    加入 引用 #include<vector> 避免出現 自動引用時出現的C++ bug vector<T> too Long這種 奇怪的Bug
    參考 https://blog.csdn.net/chunyexiyu/article/details/42457269

--- OpenCV_ExtensionTool/OpenCV_Extension_Tool.h
+++ OpenCV_ExtensionTool/OpenCV_Extension_Tool.h
@@ -4,6 +4,7 @@
 #include<opencv2/highgui/highgui.hpp>
 #include<opencv2/imgproc/imgproc.hpp> //mophorlogical operation
 #include<opencv2/core.hpp>
+#include<vector>
 
 using namespace cv;
 using namespace std;
